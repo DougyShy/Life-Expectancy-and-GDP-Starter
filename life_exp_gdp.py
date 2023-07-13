6912ac68929c14097bd438b736dc187e9563ad0c
DougyShy <90754920+DougyShy@users.noreply.github.com>
2023-07-12 19:29:20 -0500

    First Commit

---
+++ life_exp_gdp.py
@@ -0,0 +1,11 @@
+# Load libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+
+# Read in the data
+life_exp_gdp = pd.read_csv('D:/Code/Life-Expectancy-and-GDP-Starter/all_data.csv')
+print(life_exp_gdp)
+
